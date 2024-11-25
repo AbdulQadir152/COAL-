caf4ee22a6e75a921ac9b942395e0489473a7c1a
Taaha Khan <taahakhan125@gmail.com>
2024-11-25 01:07:46 -0800

    AddBook & ShowInventoryProcedure

--- Project.asm
+++ Project.asm
@@ -10,11 +10,30 @@ INCLUDE Irvine32.inc
     option6 BYTE "6. Exit Program", 0
     choice BYTE "Enter your choice: ", 0
     msgInvalid BYTE "Invalid option!", 0
+    dot BYTE " . ", 0
     memberCount DWORD 0
     members DWORD 5 DUP(?)
+    books BYTE 5 DUP(30 DUP(?))
+    author BYTE 5 DUP(30 DUP(?))
+    genre BYTE 5 DUP(30 DUP(?))
+    publicationYear DWORD 5 DUP(0)
+    bookCount DWORD 0
+    takeBookName BYTE "Enter name of the book: ", 0
+    authorName BYTE "Enter author of the book: ", 0
+    takeYear BYTE "Enter publication year: ", 0
+    takeGenre BYTE "Enter genre of the book: ", 0
+    temp DWORD 0
+    nameBook BYTE "Book Name: ", 0
+    auth BYTE "Author: ", 0
+    gnr BYTE "Genre: ", 0
+    pby BYTE "Publication Year: ", 0
+    borrowedBooks DWORD 5 DUP(0)
     msgMemberAdded BYTE "Member added successfully!", 0
+    msgBookAdded BYTE "Book added successfully!", 0
+    noSpace BYTE "Library Full.", 0
+    msgNoBooks BYTE "No books.", 0
     fullMember BYTE "Members Full.", 0
-
+    msgNoMember BYTE "No member in the library.", 0
 
 .code
 main PROC
@@ -60,6 +79,14 @@ ProcessChoice PROC
     push eax
     cmp eax, 1
     je AddMember
+    cmp eax, 2
+    je AddBook
+    cmp eax, 3
+    je ShowInventory
+    cmp eax, 4
+    je BorrowBook
+    cmp eax, 5
+    je ReturnBook
     cmp eax, 6
     je endd
     mov edx, OFFSET msgInvalid
@@ -92,8 +119,66 @@ EndAddMember:
 AddMember ENDP
 
 AddBook PROC
-    push eax
     ; Code to add a book
+    push eax
+    cmp memberCount, 0
+    je NoMembers
+    cmp bookCount, 5
+    jge BookFull
+    mov ecx, 5
+    mov esi, OFFSET books
+    mov edi, OFFSET author
+    mov ebx, OFFSET genre
+    TakingInput:
+        push ecx
+        TakingName:
+            mov edx, OFFSET takeBookName
+            call WriteString
+            mov edx,esi
+            mov ecx,30
+            call ReadString
+            add esi,30
+        TakingAuthor:  
+            mov edx, OFFSET authorName
+            call WriteString
+            mov edx, edi
+            mov ecx,30
+            call ReadString
+            add edi, 30
+        TakingGenre:  
+            mov edx, OFFSET takeGenre
+            call WriteString
+            mov edx, ebx
+            mov ecx,30
+            call ReadString
+            add ebx, 30
+        TakingYear:  
+            mov edx, OFFSET takeYear
+            call WriteString
+            call ReadDec
+        mov edx, OFFSET msgBookAdded
+        call WriteString
+        call crlf
+        mov edx, bookCount
+        inc bookCount
+        mov borrowedBooks[edx * 4], 1
+        mov publicationYear[edx * 4], eax
+        pop ecx
+        dec ecx
+        cmp ecx, 0
+        je EndAddBook
+        jmp TakingInput
+
+NoMembers:
+    mov edx, OFFSET msgNoMember
+    call WriteString
+    jmp EndAddBook
+
+BookFull:
+    ; Handle case where the book array is full
+    mov edx, OFFSET noSpace
+    call WriteString
+
 EndAddBook:
     pop eax
     call DisplayMenu
@@ -101,8 +186,59 @@ EndAddBook:
 AddBook ENDP
 
 ShowInventory PROC
-    push eax
     ; Code to show inventory
+    push eax
+    cmp memberCount, 0
+    je NoMembers
+    cmp bookCount, 0
+    je NoBooks
+
+    mov ecx, 5
+    mov esi, OFFSET books
+    mov edi, OFFSET author
+    mov ebx, OFFSET genre
+    PrintLoop:
+        mov edx, OFFSET nameBook
+        call WriteString
+        mov edx, esi
+        call WriteString
+        call crlf
+
+        mov edx, OFFSET auth
+        call WriteString
+        mov edx, edi
+        call WriteString
+        call crlf
+
+        mov edx, OFFSET gnr
+        call WriteString
+        mov edx, ebx
+        call WriteString
+        call crlf
+        mov edx, temp
+        inc temp
+        mov eax, publicationYear[edx * 4]
+        mov edx, OFFSET pby
+        call WriteString
+        call WriteDec
+        call crlf
+        call crlf
+
+        add esi, 30
+        add edi, 30
+        add ebx, 30
+        loop PrintLoop
+jmp EndShowInventory
+
+NoMembers:
+    mov edx, OFFSET msgNoMember
+    call WriteString
+    jmp EndShowInventory
+
+NoBooks:
+    mov edx, OFFSET msgNoBooks
+    call WriteString
+       
 EndShowInventory:
     pop eax
     call DisplayMenu
